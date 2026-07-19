e6c7e15217b15b777342ed1e8ea82e8054bc4ae0
Pratap Shishodia <shishodiapratap24@gmail.com>
2026-07-19 22:27:11 +0530

    Add Service Discovery

--- backend/product_service/src/main/java/com/ps/product_service/ProductServiceApplication.java
+++ backend/product_service/src/main/java/com/ps/product_service/ProductServiceApplication.java
@@ -3,10 +3,13 @@ package com.ps.product_service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @Slf4j
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 public class ProductServiceApplication {
 
--- backend/user_servcie/src/main/java/com/ps/user_service/UserServiceApplication.java
+++ backend/user_servcie/src/main/java/com/ps/user_service/UserServiceApplication.java
@@ -3,10 +3,12 @@ package com.ps.user_service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @Slf4j
+@EnableDiscoveryClient
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 public class UserServiceApplication {
 	public static void main(String[] args) {
