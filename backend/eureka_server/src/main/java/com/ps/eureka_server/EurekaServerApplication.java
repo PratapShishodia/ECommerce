f60cfbf895bbfc062cfd9bd164a7200ebe38bd97
Pratap Shishodia <shishodiapratap24@gmail.com>
2026-07-20 18:16:46 +0530

    Add Eureka Server

--- backend/eureka_server/src/main/java/com/ps/eureka_server/EurekaServerApplication.java
+++ backend/eureka_server/src/main/java/com/ps/eureka_server/EurekaServerApplication.java
@@ -1,9 +1,12 @@
 package com.ps.eureka_server;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @Slf4j
 @SpringBootApplication
@@ -11,8 +14,17 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
+		System.out.println(System.getenv("APPLICATION_PORT"));
 		SpringApplication.run(EurekaServerApplication.class, args);
 		log.info("Eureka Server Started");
+	}
+
+	@Bean
+	CommandLineRunner runner(Environment env) {
+		return args -> {
+			System.out.println("APPLICATION_PORT = " + env.getProperty("APPLICATION_PORT"));
+			System.out.println("server.port      = " + env.getProperty("server.port"));
+		};
 	}
 
 }
